60ee5ca4126dd4eb53a9e5803e61910230f7399f
Bryce <brycedartez@yahoo.com>
2019-04-02 22:22:26 -0500

    configuring for heroku

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\bryce.desktop-p36sibq\projects\mysite\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
